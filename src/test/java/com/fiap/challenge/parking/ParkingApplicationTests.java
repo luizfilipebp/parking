716106fedfe0222a4defff950aa79bc7270f82b4
Luiz Filipe Bastos Pereira <bastos.filipe@marinha.mil.br>
2024-07-18 19:37:05 -0300

    initial commit

---
+++ src/test/java/com/fiap/challenge/parking/ParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.challenge.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
