45cadf905e37216727752abab22d465e2d51ec1f
Luiz Filipe Bastos Pereira <bastos.filipe@marinha.mil.br>
2024-07-22 20:27:12 -0300

    adicionando o banco postgres com o docker compose

--- src/main/java/com/fiap/challenge/parking/dominio/condutor/entidade/Condutor.java
+++ src/main/java/com/fiap/challenge/parking/dominio/condutor/entidade/Condutor.java
@@ -1,17 +1,26 @@
 package com.fiap.challenge.parking.dominio.condutor.entidade;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "condutor")
 public class Condutor {
+    @Id
     private Long id;
     private String cpf;
     private String nome;
+    private String endereco;
+    private String telefone;
 
-    public Condutor(Long id, String cpf, String nome) {
+
+    public Condutor(Long id, String cpf, String nome, String endereco, String telefone) {
         this.id = id;
         this.cpf = cpf;
         this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
     }
 
     public Condutor() {
